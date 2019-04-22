b1be9c8fa3dc2050d008d13d47181e037d9dad71
shaginhekvs <shaginhekvs@gmail.com>
2019-04-22 19:13:23 +0200

    started_join

--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -7,6 +7,31 @@ import org.apache.spark.sql.Row
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+object Levenshtein0 extends App {
+ 
+  def distance(s1: String, s2: String): Int = {
+    val dist = Array.tabulate(s2.length + 1, s1.length + 1) { (j, i) => if (j == 0) i else if (i == 0) j else 0 }
+ 
+    @inline
+    def minimum(i: Int*): Int = i.min
+ 
+    for {j <- dist.indices.tail
+         i <- dist(0).indices.tail} dist(j)(i) =
+        if (s2(j - 1) == s1(i - 1)) dist(j - 1)(i - 1)
+        else minimum(dist(j - 1)(i) + 1, dist(j)(i - 1) + 1, dist(j - 1)(i - 1) + 1)
+ 
+    dist(s2.length)(s1.length)
+  }
+ 
+  def printDistance(s1: String, s2: String) {
+    println("%s -> %s : %d".format(s1, s2, distance(s1, s2)))
+  }
+ 
+  printDistance("kitten", "sitting")
+  printDistance("rosettacode", "raisethysword")
+ 
+}
+
 class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Serializable {
   val logger = LoggerFactory.getLogger("SimilarityJoin")
   var rdd: RDD[String] = null
@@ -16,7 +41,18 @@ class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Seriali
    * of the dataset, and returns the result of the similarity self join on
    * that attribute.
    * */  
+  
+  
   def similarity_join(dataset: Dataset, attrIndex: Int) : RDD[(String, String)] = {
+    // step 1: sampling
+    
+    val rdd = dataset.getRDD()
+    val schema = dataset.getSchema()
+    val overallRowSize = rdd.count().toInt
+    val fract = (numAnchors*1.0)/overallRowSize;
+    val rdd_anchors = rdd.sample(false, fract, 100);
+    rdd_anchors.collect().map(println );
+    println(overallRowSize)
     
     null
   }
